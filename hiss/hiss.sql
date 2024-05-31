3ac897aeed3c10bd5101c636f55bb77ca2860c20
thetrotfreak <thetrotfreak@yahoo.com>
2024-05-31 16:03:32 +0530

    feat: bootstrap flask app
    Bootstrapped from https://github.com/pallets/flask/tree/main/examples/tutorial
    Refactored database file and schema
    Like or Unlike posts

---
+++ hiss/hiss.sql
@@ -0,0 +1,28 @@
+-- Initialize the database.
+-- Drop any existing data and create empty tables.
+
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS likes;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE post (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  body TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE likes (
+  post_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  PRIMARY KEY (post_id, user_id),
+  FOREIGN KEY (post_id) REFERENCES post (id),
+  FOREIGN KEY (post_id) REFERENCES user (id)
+)
