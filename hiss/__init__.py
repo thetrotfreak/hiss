94cc49e64ca8596d2b30e9f9c20e9cf7fac3dee3
thetrotfreak <thetrotfreak@yahoo.com>
2024-05-31 22:07:22 +0530

    refactorremove //hello route
    remove the HTTP GET route '/hello'

--- hiss/__init__.py
+++ hiss/__init__.py
@@ -26,10 +26,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route("/hello")
-    def hello():
-        return "Hello, World!"
-
     # register the database commands
     from . import db
 
@@ -44,8 +40,7 @@ def create_app(test_config=None):
 
     # make url_for('index') == url_for('blog.index')
     # in another app, you might define a separate main index here with
-    # app.route, while giving the blog blueprint a url_prefix, but for
-    # the tutorial the blog will be the main index
+    # app.route, while giving the blog blueprint a url_prefix
     app.add_url_rule("/", endpoint="index")
 
     return app
